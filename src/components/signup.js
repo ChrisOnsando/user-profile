96d6f5f48c38c28503954b7d1678e09667bbb15c
ChrisOnsando <chrisnemwel@gmail.com>
2022-05-08 12:49:41 +0300

    Update included te signup markup

--- src/components/signup.js
+++ src/components/signup.js
@@ -1,9 +1,38 @@
 import React from "react";
+import sign from"../images/sign.svg";
 
 function SignUp() {
     return(
         <div className="SignUp">
-            <h1>Hello this is the sign up page</h1>
+            <div className="leftSignUp">
+                <img src={sign} alt="signup image" />
+            </div>
+            <div className="signUpForm">
+                <div className="signUpheader">
+                    <h2>GET STARTED</h2>
+                    <p>Already have an account? <a href="#">Sign In</a></p>
+                </div>
+                <div className="signupwith">
+                    <p>Sign up with Google</p>
+                    <p>Sign up with Facebook</p>
+                </div>
+                <div className="or">
+                    <span></span>
+                    <p>Or</p>
+                    <span></span>
+                </div>
+                <div className="signUpForm">
+                    <form className="form">
+                      <label for="name">Name</label>
+                      <input type="text" placeholder="your names" required/>
+                      <label for="name">Email</label>
+                      <input type="email" placeholder="your email" required/>
+                      <label for="name">Name</label>
+                      <input type="password" placeholder="your password" required/>
+                      <button type="submit">Sign Up</button>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
