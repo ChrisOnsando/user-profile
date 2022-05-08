5dd35c786afbf638975d0b14ce055abff40770ee
ChrisOnsando <chrisnemwel@gmail.com>
2022-05-08 11:55:03 +0300

    Update imported our components into app.js file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Signin from "./components/sigin";
+import SignUp from "./components/signup";
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello I am Chris</h1>
+      <SignUp />
+      <Signin />
     </div>
   );
 }
