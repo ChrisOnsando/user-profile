052b2f293fd90046d9cb39d01f8a34ef3ff27586
ChrisOnsando <chrisnemwel@gmail.com>
2022-05-08 14:41:52 +0300

    Update created and included signin css file to index file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './stylesheets/signin.css';
+import './stylesheets/app.css';
+import './stylesheets/signup.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
