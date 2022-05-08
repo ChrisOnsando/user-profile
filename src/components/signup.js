94540519ad414a990c94645c3f4f4f4d176b57e1
ChrisOnsando <chrisnemwel@gmail.com>
2022-05-08 11:46:58 +0300

    Update included the sigup component

---
+++ src/components/signup.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SignUp() {
+    return(
+        <div className="SignUp">
+            <h1>Hello this is the sign up page</h1>
+        </div>
+    )
+}
+
+export default SignUp;
