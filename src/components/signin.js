56413aad14ec2519440d9077011d0cb54b4a0e0f
ChrisOnsando <chrisnemwel@gmail.com>
2022-05-11 18:12:33 +0300

    Update :   - Included useState hook to manage project state   - Included a handlerfunction to handle user input   - Made use of the useState callback functions to manage changes in form state

--- src/components/signin.js
+++ src/components/signin.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import login from"../images/login.png";
 
 function Signin() {
+    const [email, setEmail] = useState('');
+    const [pwd, setPwd] = useState('');
+
+    function handleSignIn() {
+        
+    }
+
     return(
         <div className="signin">
             <div className="signInForm">
@@ -10,11 +17,11 @@ function Signin() {
                     <p>Welcome back! Please enter your details.</p>
                 </div>
                 <div className="signinForm">
-                    <form className="form">
+                    <form onSubmit={handleSignIn} className="form">
                       <label for="name">Email</label>
-                      <input type="email" placeholder="your email" required/>
+                      <input value={name} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="your email" required/>
                       <label for="password">Password</label>
-                      <input type="password" placeholder="**********" required/>
+                      <input value={pwd} onChange={(e) => setPwd(e.target.value)} type="password" placeholder="**********" required/>
                       <button type="submit">Sign In</button>
                       <button className="google" type="submit">Sign In with Google</button>
                     </form>
